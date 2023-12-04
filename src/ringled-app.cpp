703a6efdd6bbc8dd8e780144122bb2adb52cd03d
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-12-05 00:12:37 +0100

    wip: blink builtin led if wifi connection works

--- src/ringled-app.cpp
+++ src/ringled-app.cpp
@@ -10,25 +10,17 @@ private:
   String ssid;
   String password;
   const int CONNECTION_ATTEMPTS = 3;
+  bool wifiConnection;
 
 public:
   RingLedApp(String ssid, String password) {
     this->ssid = ssid;
     this->password = password;
-
-    Serial.println("Costruttore");
-    Serial.println(this->ssid);
-    Serial.println(this->password);
   };
 
   void setup() {
 
-    Serial.println("Setup");
-    Serial.println(this->ssid);
-    Serial.println(this->password);
-
     WiFi.begin(this->ssid.c_str(), this->password.c_str());
-
     for (int i = 0; i < CONNECTION_ATTEMPTS; i++) {
       if (WiFi.status() == WL_CONNECTED) {
         break;
@@ -37,19 +29,30 @@ public:
       Serial.println("Connecting to WiFi... Attempt " + String(i));
     }
 
+    pinMode(LED_BUILTIN, OUTPUT);
+
     if (WiFi.status() != WL_CONNECTED) {
       // Set the internal LED to red
-      pinMode(LED_BUILTIN, OUTPUT);
       digitalWrite(LED_BUILTIN, HIGH);
       Serial.println("Failed to connect to WiFi");
       return;
     }
+
+    wifiConnection = true;
 
     Serial.println("Connected to WiFi");
   }
 
   void loop() {
     Serial.println("RingLedApp loop");
-    delay(1000);
+
+    if (wifiConnection) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(500);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(500);
+    } else {
+      delay(1000);
+    }
   }
 };
