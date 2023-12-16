e9947f75f107f57454dd39b2fe7270b1fc574209
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-12-16 18:54:51 +0100

    feat: create task to detect the pressure using touch pins

---
+++ src/app-state/ringled-app/task/pressure-detection.cpp
@@ -0,0 +1,19 @@
+#include "pressure-detection.h"
+#include <Arduino.h>
+#include <app-state/ringled-app/util/pressure-handler.h>
+
+void pressureDetectionTask(void *parameter) {
+  PressureHandler *handler = (PressureHandler *)parameter;
+  while (true) {
+    if (touchRead(PRESSURE_DETECTION_PIN) < PRESSURE_DETECTION_THRESHOLD) {
+      handler->onInteractionStart();
+      // Serial.println("Touch: " + String(touchRead(PRESSURE_DETECTION_PIN)));
+      while (touchRead(PRESSURE_DETECTION_PIN) < PRESSURE_DETECTION_THRESHOLD) {
+        handler->onInteractionUpdate();
+        vTaskDelay(pdMS_TO_TICKS(25));
+      }
+      handler->onInteractionEnd();
+    }
+    vTaskDelay(pdMS_TO_TICKS(25));
+  }
+}
---
+++ src/app-state/ringled-app/task/pressure-detection.h
@@ -0,0 +1,9 @@
+#ifndef PRESSURE_DETECTION_H
+#define PRESSURE_DETECTION_H
+
+#define PRESSURE_DETECTION_PIN 12
+#define PRESSURE_DETECTION_THRESHOLD 50
+
+void pressureDetectionTask(void *parameter);
+
+#endif
