57c1721f3f1e0cc0281128a1cf71f9a725f749fe
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-12-24 12:13:44 +0100

    fix: avoid creation of mqtt task if the wifi could not connect

--- src/app-state/ringled-app/ringled-app.cpp
+++ src/app-state/ringled-app/ringled-app.cpp
@@ -29,19 +29,18 @@ RingLedApp::RingLedApp(String ssid, String password, RingLed *ringLed) {
     connectionManager = new ConnectionManager(ringLed);
     Serial.println("Connected to WiFi");
     pressureHandler = new NormalHandler(ringLed, connectionManager);
+    xTaskCreate(
+        [](void *pvParameters) {
+          ConnectionManager *connectionManager =
+              (ConnectionManager *)pvParameters;
+          while (true) {
+            connectionManager->loop();
+            vTaskDelay(pdMS_TO_TICKS(1));
+          }
+        },
+        "mqttHandlerTask", 4096, connectionManager, 1, NULL);
   }
 
   xTaskCreate(pressureDetectionTask, "pressureDetectionTask", 10000,
               pressureHandler, 1, NULL);
-
-  xTaskCreate(
-      [](void *pvParameters) {
-        ConnectionManager *connectionManager =
-            (ConnectionManager *)pvParameters;
-        while (true) {
-          connectionManager->loop();
-          vTaskDelay(pdMS_TO_TICKS(1));
-        }
-      },
-      "mqttHandlerTask", 4096, connectionManager, 1, NULL);
 }
